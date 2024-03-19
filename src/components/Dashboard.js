64165ea5e0df1c576e377a1f966e7039a1ea7733
Sagar Chaudhary <sagarchaudhary0106@gmail.com>
2024-03-19 13:01:25 +0000

    updates

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,35 +10,41 @@ function Dashboard() {
 
   function addChart(type) {
     const allCharts = JSON.parse(JSON.stringify(charts));
-    allCharts.push({ type, title:title.current.textContent, description:description.current.textContent });
+    allCharts.push({ type, title: title.current.textContent, description: description.current.textContent });
     title.current.textContent = "Title";
     description.current.textContent = "Description";
     setCharts(allCharts);
   }
 
   return (
-    <div className="row">
-        <div className="card m-3" style={{ width: 30 + "%" }}>
+    <div className="row p-0">
+      <div className="card m-3 card-size">
+        <div class="card-header">
+          Base component
+        </div>
+        <div className="card-body">
+          <h5 className="card-title" name="title" contentEditable="true" ref={title}>Title</h5>
+          <h6 className="card-subtitle mb-2 text-muted" name="description" contentEditable="true" ref={description}>Description</h6>
+          <p className="card-text">Please select a chart from below to create</p>
+          <a onClick={(e) => addChart("pie")} className="card-link create-chart">Pie Chart</a><br />
+          <a onClick={(e) => addChart("bar")} className="card-link create-chart">Single Stacked Bar Chart</a><br />
+          <a onClick={(e) => addChart("stackedBar")} className="card-link create-chart">Multi Stacked Bar Chart</a><br />
+        </div>
+      </div>
+      {charts.map((chart, index) => {
+        return <div className="card m-3 card-size">
+          <div class="card-header">
+            {index + 1}
+          </div>
           <div className="card-body">
-            <h5 className="card-title" name="title" contentEditable="true" ref={title}>Title</h5>
-            <h6 className="card-subtitle mb-2 text-muted" name="description" contentEditable="true" ref={description}>Description</h6>
-            <p className="card-text">Please select a chart from below to create</p>
-            <a onClick={(e)=> addChart("pie")} className="card-link create-chart">Pie Chart</a><br/>
-            <a onClick={(e)=> addChart("bar")} className="card-link create-chart">Single Stacked Bar Chart</a><br/>
-            <a onClick={(e)=> addChart("stackedBar")} className="card-link create-chart">Multi Stacked Bar Chart</a><br/>
+            <h5 className="card-title">{chart.title}</h5>
+            <h6 className="card-subtitle mb-2 text-muted">{chart.description}</h6>
+            {chart.type === "pie" && <CustomPieChart></CustomPieChart>}
+            {chart.type === "bar" && <CustomBarChart></CustomBarChart>}
+            {chart.type === "stackedBar" && <CustomStackedBarChart></CustomStackedBarChart>}
           </div>
         </div>
-        {charts.map(chart => {
-           return <div className="card m-3" style={{ width: 30 + "%" }}>
-            <div className="card-body">
-              <h5 className="card-title">{chart.title}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">{chart.description}</h6>
-             {chart.type === "pie" && <CustomPieChart></CustomPieChart>}
-             {chart.type === "bar" && <CustomBarChart></CustomBarChart>}
-             {chart.type === "stackedBar" && <CustomStackedBarChart></CustomStackedBarChart>}
-              </div>
-        </div>
-          })}
+      })}
     </div>
   )
 }
